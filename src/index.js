53adc8cb2b60774e723e6090869c460ef8adc65a
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-09-14 22:49:01 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+var shech = 0;
+
+for (var i = 0; i<preferences.length; i++) {
+  var firstChel = i;
+  var secondChel = preferences[i] - 1;
+  if (firstChel == secondChel){
+    continue;
+  }
+  var thirdChel = preferences[secondChel] - 1;
+  
+  if (firstChel == (preferences[thirdChel] - 1)) {
+    shech++;
+  }
+}
+
+var ret = shech/3;
+return ret;
 };
